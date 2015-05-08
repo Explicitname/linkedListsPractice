4ad1810d6b22a9ce8be5575393d7d805e3c3766d
Explicitname <explicit6911@gmail.com>
2015-05-08 13:43:12 -0400

    First commit

---
+++ linkedListsPractice.cpp
@@ -0,0 +1,72 @@
+//#include "stdafx.h"
+
+#include <iostream>
+#include <new>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::cin;
+
+struct Node{
+public:
+	Node();
+	Node(int value);
+	int printVal();
+
+private:
+	int _value;
+	Node *_nextNode;
+};
+
+Node::Node(){
+	_nextNode	= NULL;
+	_value		= NULL;
+}
+
+Node::printVal(){
+	return _value;
+}
+
+Node::Node(int value){
+	_nextNode	= NULL;
+	_value		= value;
+}
+
+
+class LinkedList{
+public:
+	LinkedList();
+	//~LinkedList();
+	void addNodeBegin(int value);
+private:
+	int _length = 0;
+	Node *_listHead;
+	Node *_listTail;
+};
+
+LinkedList::LinkedList(){
+	_listHead = NULL;
+	_listTail = NULL;
+	_length   = 0;
+}
+
+void LinkedList::addNodeBegin(int value){
+	if (_listHead == NULL){
+		Node *newNode = new Node(value);
+		_length += 1;
+		_listHead = newNode;
+		_listTail = newNode;
+	}
+	else{
+		Node *newNode = new Node(value);
+		_length += 1;
+		newNode->_nextNode = _listHead;
+		_listHead = newNode;
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+}
